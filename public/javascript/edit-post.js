40b74ce5b0a8b9c2abef96938aba26e8bd12dbf2
Micah Duehring <micah@theduehrings.com>
2021-11-14 14:54:50 -0600

    fixed styling

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,30 +1,27 @@
 async function editFormHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
+  const subject = document.querySelector('input[name="post-subject"]').value
+  const content=document.querySelector('input[name="post-body"]').value
+  const id=window.location.toString().split("/") [
+    window.location.toString().split("/").length - 1
+  ]
   
-    const post_date = document.querySelector('input[name="post-date"]').value;
-    const post_day = document.querySelector('input[name="post-day"]').value;
-    const post_time = document.querySelector('input[name="post-time"]').value;
-    const id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
-      ];
-  
-    const response = await fetch(`/api/posts/${id}`, {
-        method: 'PUT',
-        body: JSON.stringify({
-            post_date,
-            post_day,
-            post_time
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-      });
-      
-      if (response.ok) {
-        document.location.replace('/dashboard/');
-      } else {
-        alert(response.statusText);
-      }
+  const response = await fetch(`/api/posts/${id}`, {
+    method:'PUT',
+    body:JSON.stringify({
+      subject: subject,
+      post_body: content
+    }),
+    headers:{
+      "Content-Type":"application/json"
+    }
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard/');
+  } else {
+    alert(response.statusText);
   }
-  
-  document.querySelector('#edit-post-form').addEventListener('submit', editFormHandler);
+}
+
+document.querySelector("#edit-post-form").addEventListener("submit", editFormHandler);
